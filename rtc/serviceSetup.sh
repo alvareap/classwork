b82339627795024a8daf9d0c923ad97fe9d51acb
Alex Alvarez <alvareap@rose-hulman.edu>
2013-12-10 20:02:16 -0800

    rtc service files

---
+++ rtc/serviceSetup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# By Alex Alvarez, alvarep@rose-hulman.edu
+# This script creates a new directory if passed as the parameter, and places the rtc script and service files in those given directories. It also manipulates the service file such that the working directories change with the given directory parameter.
+# If no paramter is given the working directory is used as the service location
+
+if [ "$1" ]; then
+
+  DIR=$2
+  $(mkdir $2)
+  
+
+else
+
+DIR=$(pwd)
+  
+fi
+
+
+SED_ARG="-e 's@WorkingDirectory=.*@WorkingDirectory=$DIR@' rtc-ds1307.service" 
+
+eval sed "$SED_ARG" > tmp.txt
+
+mv tmp.txt rtc-ds1307.service
+
+
+
+cp rtc-ds1307.service $DIR
+cp clock_init.sh $DIR
